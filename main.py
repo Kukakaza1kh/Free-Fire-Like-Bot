1cc3c04a921c7b6e8a84a4561c7a31af85436aab
Kukakaza1kh <166289768+Kukakaza1kh@users.noreply.github.com>
2026-05-22 04:24:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import requests
 import time
 import threading
 from datetime import datetime, timedelta
-
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import
+InlineKeyboardMarkup, InlineKeyboardButton
 from flask import Flask, request, jsonify
 import logging
 import sys
+#
 # ╔══════════════════════════════════════════════════════════════════╗
 # ║  CREATOR: TARIKUL ISLAM
 # ║  TELEGRAN: https://t.me/paglu_dev
